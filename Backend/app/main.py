a24d79bdf327012a03f71ad3429be6a174972280
Ajithkar <pajithkar@gmail.com>
2026-07-06 12:12:39 +0530

    port number changed

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",  # React (Vite)
-        "http://127.0.0.1:5173",
+        "http://127.0.0.1:8000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
