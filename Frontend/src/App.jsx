89860375cf4feafb7ab21d5034f0649a1daa4f73
kobika525 <kobishankar05@gmail.com>
2026-07-06 14:12:13 +0530

    App.jsx error resolve

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,21 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import './App.css'
 import Registration from './pages/registration'
 import Login from './pages/login'
 import AddCourse from './pages/AddCourse'
-import Profile from './pages/profile'
+import Profile from './pages/Profile'
 import Courses from './pages/courses'
 import Dashboard from './pages/Dashboard'
-
+import Student from './pages/Student'
+import EditCourse from './pages/EditCourse'
 
  
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Navigate to="/Login" replace />} />
       <Route path="/register" element={<Registration />} />
       <Route path="/Login" element={<Login />} />
       <Route path="/Profile" element={<Profile/>} />
@@ -24,9 +24,7 @@ function App() {
       <Route path="/Dashboard" element={<Dashboard/>} />
       <Route path="/Student" element={<Student/>} />
       <Route path="/EditCourse" element={<EditCourse/>} />
-
-      
-
+      <Route path="*" element={<Navigate to="/Login" replace />} />
 
     </Routes>
   </BrowserRouter>
