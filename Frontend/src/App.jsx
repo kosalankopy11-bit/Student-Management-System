c88c456c1f71f3ef4e771a22fc29cd50dfdef5bc
kobika525 <kobishankar05@gmail.com>
2026-07-06 14:00:59 +0530

    Bug resolve

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Registration from './pages/registration'
 import Login from './pages/login'
+import AddCourse from './pages/AddCourse'
+import Profile from './pages/profile'
+import Courses from './pages/courses'
+import Dashboard from './pages/Dashboard'
+
 
  
 function App() {
@@ -11,7 +16,18 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Registration />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Profile" element={<Profile/>} />
+      <Route path="/AddCourse" element={<AddCourse/>} />
+      <Route path="/Courses" element={<Courses/>} />
+      <Route path="/Dashboard" element={<Dashboard/>} />
+      <Route path="/Student" element={<Student/>} />
+      <Route path="/EditCourse" element={<EditCourse/>} />
+
+      
+
+
     </Routes>
   </BrowserRouter>
   )
